93de0185527237b0b07472f20a749c536f1036b1
burddan <faithsteam8818@gmail.com>
2026-03-12 18:55:22 -0300

    migracao para o php junto e divisao footer e header

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 projeto thinkpad
+
+
+TODO:
+    home:
+        1 - centralizar certinho a div das imagens HOME
+        2 - aumentar o menu Thinkpad Archive
+    
+
+    final
+        arrumar toda a estrutura/ variaveis do codigo
---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+          <a>
+            <img src="https://88x31.nl/gifs/bikobatanari.gif">
+          </a>
+          <a>
+            <img src="https://88x31.nl/gifs/debian.gif">
+          </a>
+          <a>
+            <img src="https://88x31.nl/gifs/dawa.gif">
+          </a>
+          <a>
+            <img src="https://88x31.nl/gifs/discord2.gif">
+          </a>
+        </footer>
+
